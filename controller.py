00d51d5ab99ad26d76fb443cc41e57b95b5dc325
Tobias Kantusch <git@kantusch.dev>
2022-07-06 10:27:26 +0200

    Avoid forwarding loops by adding STP
    Previously, when switches were connected in a loop,
    packets would be forwarded indefinitely, as every switch
    would flood the packets into all directions

--- controller.py
+++ controller.py
@@ -13,24 +13,26 @@ from ryu.base.app_manager import RyuApp
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
+from ryu.lib import stplib
 from ryu.lib.packet import packet, ethernet
-from ryu.lib.dpid import dpid_to_str
 
 
 class Controller(RyuApp):
 
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    # By setting _CONTEXTS, we can receive instances via the kwargs argument in __init__
+    _CONTEXTS = {'stplib': stplib.Stp}
 
     def __init__(self, *args, **kwargs):
         super(Controller, self).__init__(*args, **kwargs)
 
         # Stores the mapping from destination mac to outgoing switch port
         self.mac_port_mapping = dict()
+        self.stplib = kwargs['stplib']
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def features_handler(self, ev):
-        '''
-        Handshake: Features Request Response Handler
+        '''Handshake: Features Request Response Handler
 
         Installs a low level (0) flow table modification that pushes packets to
         the controller. This acts as a rule for flow-table misses.
@@ -39,10 +41,7 @@ class Controller(RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        self.logger.info("Handshake taken place with {}".format(dpid_to_str(datapath.id)))
-
-        # Initialize mapping for datapath
-        self.mac_port_mapping[dpid_to_str(datapath.id)] = {}
+        self.logger.info("Handshake taken place with {}".format(datapath.id))
 
         self.__add_flow(
             datapath,
@@ -53,10 +52,9 @@ class Controller(RyuApp):
             [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)],
         )
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    @set_ev_cls(stplib.EventPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
-        '''
-        Packet In Event Handler
+        '''Packet In Event Handler
 
         Takes packets provided by the OpenFlow packet in event structure and
         learns from the packet information where following packets belong to.
@@ -66,7 +64,7 @@ class Controller(RyuApp):
         '''
         msg = ev.msg
         datapath = msg.datapath
-        dpid = dpid_to_str(datapath.id)
+        dpid = datapath.id
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
@@ -81,6 +79,7 @@ class Controller(RyuApp):
             self.logger.debug("{}: Packet from {} on port {} to {}".format(dpid, src, in_port, dst))
 
             # "Learn" port for src MAC address
+            self.mac_port_mapping.setdefault(dpid, {})
             self.mac_port_mapping[dpid][src] = in_port
             self.logger.info("{}: {} is at port {}".format(dpid, src, in_port))
 
@@ -109,9 +108,27 @@ class Controller(RyuApp):
         self.logger.info("Sending packet out")
         datapath.send_msg(out)
 
-    def __add_flow(self, datapath, priority, match, actions, timeout=0):
+    @set_ev_cls(stplib.EventTopologyChange, MAIN_DISPATCHER)
+    def topology_change_handler(self, ev):
+        '''STP Topology Changed Handler
+
+        Handles topology changes detected by the stplib and flushes the MAC
+        port mapping table accordingly.
         '''
-        Install Flow Table Modification
+        dp = ev.dp
+        self.logger.info("{}: Topology changed, flushing MAC table".format(dp.id))
+
+        if dp.id in self.mac_port_mapping:
+            self.__delete_mac_flows(dp)
+            del self.mac_port_mapping[dp.id]
+
+    @set_ev_cls(stplib.EventPortStateChange, MAIN_DISPATCHER)
+    def port_state_change_handler(self, ev):
+        '''Handle changes to a ports STP state'''
+        self.logger.info("STP {}: {} -> {}".format(ev.dp.id, ev.port_no, ev.port_state))
+
+    def __add_flow(self, datapath, priority, match, actions, timeout=0):
+        '''Install Flow Table Modification
 
         Takes a set of OpenFlow Actions and a OpenFlow Packet Match and creates
         the corresponding Flow-Mod. This is then installed to a given datapath
@@ -121,5 +138,23 @@ class Controller(RyuApp):
         parser = datapath.ofproto_parser
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst, idle_timeout=timeout)
-        self.logger.info("Flow-Mod written to {}".format(dpid_to_str(datapath.id)))
+        self.logger.info("Flow-Mod written to {}".format(datapath.id))
         datapath.send_msg(mod)
+
+    def __delete_mac_flows(self, datapath):
+        '''Delete all stored MAC-Port mapping flows for a datapath.'''
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        for dst in self.mac_port_mapping[datapath.id].keys():
+            # Match the destination we stored in the mapping
+            match = parser.OFPMatch(eth_dst=dst)
+            # Remove all flows for the datapath that match the MAC we stored
+            mod = parser.OFPFlowMod(
+                datapath,
+                command=ofproto.OFPFC_DELETE,
+                # Use any outgoing port, in case the controllers mapping changed in between
+                out_port=ofproto.OFPP_ANY,
+                match=match,
+            )
+            datapath.send_msg(mod)
--- topology.py
+++ topology.py
@@ -36,9 +36,37 @@ class TutorialTopologyAdvanced(Topo):
             self.addLink(s2, hosts[h+1], cls=TCLink, bw=80, delay='35ms')
 
 
+class RingTopology(Topo):
+
+    def build(self):
+        # Add left switch
+        s1 = self.addSwitch('s1')
+
+        # Add right switch
+        s2 = self.addSwitch('s2')
+        self.addLink(s1, s2)
+
+        # Add connecting switch
+        s3 = self.addSwitch('s3')
+        self.addLink(s3, s1)
+        self.addLink(s3, s2)
+
+        # connect n*2 hosts to the switches
+        switches = [s1,s2,s3]
+        hosts_per_switch = 2
+        hosts = []
+        for i in range(hosts_per_switch * len(switches)):
+            sid = i % len(switches)
+            host = self.addHost(f'h{i}')
+            print(host)
+            hosts.append(host)
+            self.addLink(switches[sid], host, cls=TCLink)
+
+
 # the topologies accessible to the mn tool's `--topo` flag
 # note: if using the Dockerfile, this must be the same as in the Dockerfile
 topos = {
     'tutorialTopology': (lambda: TutorialTopology()),
-    'tutorialTopologyAdvanced': (lambda: TutorialTopologyAdvanced())
+    'tutorialTopologyAdvanced': (lambda: TutorialTopologyAdvanced()),
+    'ringTopology': (lambda: RingTopology()),
 }
